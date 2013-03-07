c69633f2036e96fc8b0f85096dc4ee6719da3229
Findlay Yeates <findlay.yeates@deitek.co.uk>
2013-03-07 10:36:58 +0000

    Updated common functions and allowed support for styling

--- highcharts/common.py
+++ highcharts/common.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
-# Base Formatters
+""" PyHighcharts common.py
+Common Functions For PyHighcharts
+"""
 
 FORMATTER_TYPE_MAPPINGS = {
-	"default": "function() { return this.value }",
-	"date": "function() { return''+Highcharts.dateFormat('%e. %b %Y %H:00:00',this.x) + ': '+ this.y; }",
-	"pie": "function() { return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %'; }",
-	"pound_yAxis": "function() { '&#163' + return this.value }",
-	"pound_tooltip": "function() { return''+ this.x + ': '+ '&#163' +this.y; }"
+    "default": "function() { return this.value }",
+    "date": "function() { return''+Highcharts.dateFormat('%e. %b %Y \
+        %H:00:00',this.x) + ': '+ this.y; }",
+    "pie": "function() { return '<b>'+ this.point.name +'</b>: '+ \
+    this.percentage +' %'; }",
+    "pound_yAxis": "function() { '&#163' + return this.value }",
+    "pound_tooltip": "function() { return''+ this.x + ': '+ '&#163' +this.y; }",
+    "percent": "function() { return this.value + ' %' }",
+    "default_tooltip": "function () { return'<b>'+ this.series.name + '</b>: ' + this.y; }",
+    "percent_tooltip": "function () { return'<b>'+ this.series.name + '</b>: ' + this.y + ' %'; }",
+    "date_percent_tooltip": "function () { return''+Highcharts.dateFormat('%e. %b %Y',this.x) + '<br><b>'+ this.series.name + '</b>: ' + this.y + ' %'; }",
 }
 
 class Formatter(object):
+    """ Base Formatter Class """
 
-	def __init__(self,type):
-		self.__dict__.update({'formatter':FORMATTER_TYPE_MAPPINGS[type]})
+    def __init__(self, format_type):
+        self.__dict__.update({
+            'formatter':FORMATTER_TYPE_MAPPINGS[format_type]
+            })
