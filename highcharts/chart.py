517a857bc3df220551ad85621342145e6d125876
fidyeates <findlay.yeates@deitek.co.uk>
2013-12-09 15:54:51 +0000

    Update chart.py

--- highcharts/chart.py
+++ highcharts/chart.py
@@ -38,6 +38,7 @@ DEFAULT_POINT_INTERVAL = 86400000
 
 FORMAT_SPECIAL_CASES = {
     "formatter": "formatter",
+    "lineFormatter": "formatter",
     "pointStart": "skip_quotes",
     "events": "skip_quotes",
     "load": "skip_quotes",
