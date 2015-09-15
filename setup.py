0b3e135b8b48bff27fb31e1725744987d3734561
Chris Bunney <chris.bunney@dsp.io>
2015-09-15 13:48:26 +0100

    Fixed issue with template files not being available in expected location
    Signed-off-by: Chris Bunney <chris.bunney@dsp.io>

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='PyHighcharts',
     version='1.0',
-    packages=['PyHighcharts'],
+    packages=['PyHighcharts', 'PyHighcharts.highcharts'],
     url='https://github.com/fidyeates/PyHighcharts',
     license='PSF',
     author='Fin Yeates',
     author_email='findlay.yeates@deitek.co.uk',
-    description='A python wrapper for the highcharts charting library'
+    description='A python wrapper for the highcharts charting library',
+    include_package_data=True,
+    package_data={'PyHighcharts': ['templates/*.tmp']}
 )
